a31a13c67324310a1707b804db8775b82b2cd88c
mopin1 <397756095@qq.com>
2021-01-03 23:09:14 +0800

    interview_04_04

---
+++ src/Soluton_interview_04_04.java
@@ -0,0 +1,23 @@
+class Solution_interview_04_04 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+        return Math.abs(getDepth(root.left) - getDepth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
+    }
+}
