e5b6c8a2f55d4cae5d8b097080ede96e574c33d0
mopin1 <397756095@qq.com>
2021-01-09 19:59:01 +0800

    37

---
+++ src/Solution_37.java
@@ -0,0 +1,29 @@
+class Solution_37 {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1])
+            i--;
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j])
+                j--;
+            swap(nums, i, j);
+        }
+        reserve(nums, i + 1);
+    }
+
+    private void reserve(int[] nums, int start) {
+        int end = nums.length - 1;
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
