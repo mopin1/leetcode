45f7a8e67b966a5f9c45a030ebcb2a2275a19c2e
mopin1 <397756095@qq.com>
2021-01-11 14:25:03 +0800

    1202  并查集

---
+++ src/Solution_1202.java
@@ -0,0 +1,96 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Solution_1202 {
+
+    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+        if (pairs.size() == 0) {
+            return s;
+        }
+
+        // 第 1 步：将任意交换的结点对输入并查集
+        int len = s.length();
+        UnionFind unionFind = new UnionFind(len);
+        for (List<Integer> pair : pairs) {
+            int index1 = pair.get(0);
+            int index2 = pair.get(1);
+            unionFind.union(index1, index2);
+        }
+
+        // 第 2 步：构建映射关系
+        char[] charArray = s.toCharArray();
+        // key：连通分量的代表元，value：同一个连通分量的字符集合（保存在一个优先队列中）
+        Map<Integer, PriorityQueue<Character>> hashMap = new HashMap<>(len);
+        for (int i = 0; i < len; i++) {
+            int root = unionFind.find(i);
+            if (hashMap.containsKey(root)) {
+                hashMap.get(root).offer(charArray[i]);
+            } else {
+                 PriorityQueue<Character> minHeap = new PriorityQueue<>();
+                 minHeap.offer(charArray[i]);
+                 hashMap.put(root, minHeap);
+            }
+        }
+
+        // 第 3 步：重组字符串
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            int root = unionFind.find(i);
+            stringBuilder.append(hashMap.get(root).poll());
+        }
+        return stringBuilder.toString();
+    }
+
+    private static class UnionFind {
+
+        //parent : 用于记录该节点的父节点，所有父节点相同的节点位于同一连通图
+        private final int[] parent;
+
+        //rank : 节点的秩，主要记录该节点目前位于的树的深度，从子节点出发
+        //       主要用于优化，在合并两个父节点时，通过rank的大小判断谁父谁子
+        private final int[] rank;
+
+        public UnionFind(int n) {
+            this.parent = new int[n];
+            this.rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                this.parent[i] = i;
+                this.rank[i] = 1;
+            }
+        }
+
+        // 合并两个节点
+        // 如果处于同一个并查集， 不需要合并
+        // 如果不处于同一个并查集，判断两个rooX和rootY谁的秩大
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) {
+                return;
+            }
+
+            if (rank[rootX] == rank[rootY]) {
+                parent[rootX] = rootY;
+                // 此时以 rootY 为根结点的树的高度仅加了 1
+                rank[rootY]++;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+                // 此时以 rootY 为根结点的树的高度不变
+            } else {
+                // 同理，此时以 rootX 为根结点的树的高度不变
+                parent[rootY] = rootX;
+            }
+        }
+
+        // 路径压缩， 遍历过程中的所有父节点直接指向根节点，
+        // 减少后续查找次数
+        public int find(int x) {
+            if (x != parent[x]) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+    }
+}
