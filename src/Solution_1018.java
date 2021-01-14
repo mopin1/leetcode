24fc49132ccefbb41bd2f3e1cadf2e47abd1198d
mopin1 <397756095@qq.com>
2021-01-14 22:13:01 +0800

    1018

---
+++ src/Solution_1018.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution_1018 {
+    public List<Boolean> prefixesDivBy5(int[] A) {
+        List<Boolean> list = new ArrayList<Boolean>();
+        int prefix = 0;
+        int length = A.length;
+        for (int i = 0; i < length; i++) {
+            prefix = ((prefix << 1) + A[i]) % 5;
+            list.add(prefix == 0);
+        }
+        return list;
+    }
+}
