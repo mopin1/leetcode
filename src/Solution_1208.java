0695e42792ff69c9c0e4487e58134a154f3ab7ad
mopin1 <odadaliu@gmail.com>
2021-02-05 11:42:44 +0800

    1208

--- src/Solution_1208.java
+++ src/Solution_1208.java
@@ -1,22 +1,20 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
         int n = s.length();
-        int[] diff = new int[n];
+        int[] cost = new int[n];
         for (int i = 0; i < n; i++) {
-            diff[i] = Math.abs(s.charAt(i) - t.charAt(i));
+            cost[i] = Math.abs(s.charAt(i) - t.charAt(i));
         }
-        int maxLength = 0;
         int start = 0, end = 0;
         int sum = 0;
         while (end < n) {
-            sum += diff[end];
-            while (sum > maxCost) {
-                sum -= diff[start];
-                start++;
+            sum += cost[end];
+            if (sum > maxCost) {
+                sum -= cost[start];
+                start += 1;
             }
-            maxLength = Math.max(maxLength, end - start + 1);
-            end++;
+            end += 1;
         }
-        return maxLength;
+        return end - start;
     }
 }
