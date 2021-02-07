22a52b5912982ff5f7b53f0c735bd8312b559370
mopin1 <odadaliu@gmail.com>
2021-02-07 16:34:33 +0800

    665

---
+++ src/Soluttion_665.java
@@ -0,0 +1,17 @@
+class Solution_665 {
+    public boolean checkPossibility(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                count++;
+                if (count > 1) {
+                    return false;
+                }
+                if (i > 0 && nums[i + 1] < nums[i - 1]) {
+                    nums[i + 1] = nums[i];
+                }
+            }
+        }
+        return true;
+    }
+}
