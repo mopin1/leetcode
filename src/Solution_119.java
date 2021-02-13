15e47c9a6226b68a44e6d6fde937725789b56127
mopin1 <odadaliu@gmail.com>
2021-02-13 20:33:28 +0800

    119

---
+++ src/Solution_119.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution_119 {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> pre = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            List<Integer> cur = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    cur.add(1);
+                } else {
+                    cur.add(pre.get(j - 1) + pre.get(j));
+                }
+            }
+            pre = cur;
+        }
+        return pre;
+    }
+}
